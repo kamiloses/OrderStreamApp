63cfa610de0928a976ad2fbaeca07f987a7533d9
kamil <kamil04@gmail.com>
2024-08-13 17:01:43 +0200

    Initialize Commit

---
+++ src/test/java/com/group/kamiloses/orderstreamapp/OrderStreamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.kamiloses.orderstreamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStreamAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
