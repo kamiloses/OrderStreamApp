6c58bc1a8fa3a0b78a708a094f005f104357abc7
kamil <kamil04@gmail.com>
2024-08-13 17:08:46 +0200

    Initial commit

---
+++ src/main/java/com/group/kamiloses/orderstreamapp/OrderStreamAppApplication.java
@@ -0,0 +1,13 @@
+package com.group.kamiloses.orderstreamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStreamAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderStreamAppApplication.class, args);
+    }
+
+}
